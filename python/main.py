b2a102c182a2a19ece74cbc03ce4dc12a956edbc
raulMorales <mechar09@gmail.com>
2023-01-11 01:32:03 -0600

    new

--- python/main.py
+++ python/main.py
@@ -14,25 +14,25 @@ disponible = {
 #     return total
 
 
-# def retirar(monto):
-#     if monto > total_disponible():
-#         print("Error, no hay suficiente efectivo")
+def retirar(monto):
+    if monto > total_disponible():
+        print("Error, no hay suficiente efectivo")
     
-#     saldo = monto
-#     while saldo > 0:
-#         for denominacion, v in disponible.items():
-#             cantidad = get_denominacion(denominacion, saldo)
-#             print("Billete o moneda de: ", denominacion, cantidad)
-#             saldo -= cantidad * denominacion
+    saldo = monto
+    while saldo > 0:
+        for denominacion, v in disponible.items():
+            cantidad = get_denominacion(denominacion, saldo)
+            print("Billete o moneda de: ", denominacion, cantidad)
+            saldo -= cantidad * denominacion
         
 
-# def get_denominacion(denominacion, saldo):
-#     cantidad = saldo/denominacion
-#     if (cantidad > disponible[denominacion]):
-#         cantidad = disponible[denominacion]
+def get_denominacion(denominacion, saldo):
+    cantidad = saldo/denominacion
+    if (cantidad > disponible[denominacion]):
+        cantidad = disponible[denominacion]
     
-#     disponible[denominacion] -= cantidad
-#     return cantidad
+    disponible[denominacion] -= cantidad
+    return cantidad
 
 
-# retirar(1300)
+retirar(1300)
